d04b1e18d71db74c4b848a6abb99714e1aa559a7
SergBulchak <s.bulchak@ukr.net>
2023-02-21 12:39:46 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start');
+const stopBtn = document.querySelector('button[data-stop');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  setSwitcherInterval();
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
+
+setCurrentColor();
+
+function setSwitcherInterval() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    localStorage.setItem('current-color', getRandomHexColor());
+  }, 1000);
+}
+
+function setCurrentColor() {
+  document.body.style.backgroundColor = localStorage.getItem('current-color');
+  localStorage.clear();
+}
